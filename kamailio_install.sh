f13f4e299aa8781fa27813d3e3b40f871da4028e
Brou kouadio Stéphane-Fabien <fabienbrou99@gmail.com>
2024-12-06 12:30:23 +0000

    Check if the script is run as root

--- kamailio_install.sh
+++ kamailio_install.sh
@@ -11,3 +11,10 @@ welcome_message() {
     color_yellow "## Welcome to the Kamailio 5.8 installation script."
 }
 
+## Check if the script is run as root
+check_root() {
+    if [[ $EUID -ne 0 ]]; then
+        color_red ":: XX This script must be run with root privileges. Please try again with 'sudo'."
+        exit 1
+    fi
+}
