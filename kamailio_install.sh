19f29b8c2480e974b91c115f0956b0af46f7ffc8
Brou kouadio Stéphane-Fabien <fabienbrou99@gmail.com>
2024-12-06 12:29:18 +0000

    Function to get colored text

--- kamailio_install.sh
+++ kamailio_install.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+
+## Function to get colored text
+color_red() { echo -e "\e[31m$1\e[0m"; }
+color_green() { echo -e "\e[32m$1\e[0m"; }
+color_yellow() { echo -e "\e[33m$1\e[0m"; }
+color_blue() { echo -e "\e[34m$1\e[0m"; }
